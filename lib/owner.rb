d1408183a699012baf3bb58bc1a7e969f98261a6
Camiyah Goins <tokyonote9@gmail.com>
2020-05-13 20:26:14 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -7,38 +7,53 @@ class Owner
     @@all << self
     puts self
   end
+  
   def save
     @@all << self
   end
+  
   def self.create(name)
     owner = new(name)
     owner.save
   end
+  
   def self.all
     @@all
   end
+  
   def self.count
     all.length
   end
+  
   def self.reset_all
     all.clear
   end
+  
   def say_species
     puts "I am a #{species}."
     "I am a #{species}."
   end
+  
   def cats
     Cat.all.select { |cat| cat.owner == self }
   end
+  
   def dogs
     Dog.all.select { |dog| dog.owner == self }
- end
+  end
+  
+  def pets
+    Dog.all.Cat.all.select { |pet| pet.owner == self }
+  end
+ 
   def buy_cat(name)
     Cat.new(name, self)
   end
+  
   def buy_dog(name)
     Dog.new(name, self)
   end
+  
   def walk_dogs
     dogs.each { |dog| dog.mood = 'happy' }
   end
