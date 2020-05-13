2393ec27c2fc51ede38500846b329a5da840c680
Camiyah Goins <tokyonote9@gmail.com>
2020-05-13 20:17:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,3 @@
-# frozen_string_literal: true
-class Cat
-  attr_reader :name
-  attr_accessor :owner, :mood
-  @@all = []
-  def initialize(name, owner)
-    @name = name
-    @owner = owner
-    @mood = 'nervous'
-    @@all << self
-  end
-  def self.all
-    @@all
-  end
-  def self.cats_by_owner(owner)
-    all.select { |cat| cat.owner == owner }
-  end
-end
 class Dog
   attr_reader :name
   attr_accessor :owner, :mood
